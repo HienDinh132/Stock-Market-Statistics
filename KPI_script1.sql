2e5bcc6c7add19db4c1c2822bac68e0b166e7e71
HienDinh132 <151005162+HienDinh132@users.noreply.github.com>
2025-10-06 09:12:17 +0700

    Add files via upload

---
+++ KPI_script1.sql
@@ -0,0 +1,44 @@
+﻿with step1 as
+(
+SELECT 
+*
+FROM
+    (
+        SELECT f.Ma_ck, f.Ngay, f.Phan_loai, f.[Value_nghin_vnd]
+        FROM fact_gtm f
+    ) AS src
+PIVOT
+(
+    SUM([Value_nghin_vnd])
+    FOR Phan_loai IN ([Tổ chức nước ngoài], [Tổ chức trong nước], [Cá nhân nước ngoài], [Cá nhân trong nước])
+) AS pvt
+), 
+step2 as 
+(
+SELECT 
+*
+FROM
+    (
+        SELECT ftd.Ma_ck, ftd.Gia_tri_mua_trieu_vnd, ftd.Ngay, ftd.Phan_loai
+        FROM fact_tu_doanh ftd
+    ) AS src
+PIVOT
+(
+    SUM(Gia_tri_mua_trieu_vnd)
+    FOR Phan_loai IN ([Tu doanh])
+) AS pvt
+),
+step3 as
+(
+select step1.Ma_ck as [Mã chứng khoán], DATEFROMPARTS(YEAR(step1.Ngay), Month(step1.Ngay), Day(step1.Ngay)) as [Ngày],
+step1.[Tổ chức nước ngoài], step1.[Tổ chức trong nước], step1.[Cá nhân nước ngoài], step1.[Cá nhân trong nước], step2.[Tu doanh] as [Tự doanh]
+from step1 
+left join step2 on step1.Ma_ck = step2.Ma_ck and step1.Ngay = step2.Ngay
+where step2.Ma_ck is not null
+)
+select step3.[Ngày], step3.[Mã chứng khoán], step4.Price as [Giá], 
+step3.[Cá nhân nước ngoài], step3.[Cá nhân trong nước], step3.[Tổ chức nước ngoài], step3.[Tổ chức trong nước], step3.[Tự doanh]
+from step3
+left join (select * from fact_gia) step4
+on step3.[Ngày] = step4.Ngay and step3.[Mã chứng khoán] = step4.Ma_ck
+order by step3.[Ngày]
---
+++ KPI_script2.sql
@@ -0,0 +1,46 @@
+﻿with step1 as
+(
+select * from (  
+select t.[Ngay] as Ngay, t.Ma_ck, t.Price, t.Price_previous_day,  
+Round(((t.Price - t.Price_previous_day) / t.Price_previous_day),4) as [% tang_gia]  
+from (  
+select f.Ngay, f.[Ma_ck], f.Price,  
+LAG (Price, 1, 0)   
+OVER (PARTITION BY Ma_ck ORDER BY [Ngay]) AS Price_previous_day  
+from fact_gia f ) t  
+where t.Price_previous_day != 0 ) g
+),
+step2_1 as
+(
+select dn.Ma_ck, d1.ten_nganh as phan_nganh_lv1
+from dim_phan_nganh dn
+left join
+(select * from dim_nganh_lv1) d1
+on dn.id_phan_nganh = d1.id
+where phan_loai like '%1'
+),
+step2_2 as
+(
+select dn.Ma_ck, d5.ten_nganh as phan_nganh_lv5
+from dim_phan_nganh dn
+left join
+(select * from dim_nganh_lv5) d5
+on dn.id_phan_nganh = d5.id
+where phan_loai like '%5'
+)
+,
+step2 as
+(
+select a.Ma_ck, a.phan_nganh_lv1, b.phan_nganh_lv5 
+from step2_1 as a
+left join step2_2 as b
+on a.Ma_ck = b.Ma_ck
+)
+select DATEFROMPARTS(year(step1.Ngay), MONTH(step1.Ngay), Day(step1.Ngay)) as [Ngày],
+step1.Ma_ck as [Mã chứng khoán], step2.[phan_nganh_lv1] as [Phân ngành cấp 1], step2.phan_nganh_lv5 as [Phân ngành cấp 5],
+step1.Price as [Giá], step1.Price_previous_day as [Giá phiên trước], 
+Cast((step1.[% tang_gia] * 100) as varchar) + ' %' as [% tăng giá]
+from step1
+left join step2 
+on step1.Ma_ck = step2.Ma_ck
+order by step1.Ngay desc, step2.[phan_nganh_lv1] desc, step2.[phan_nganh_lv5] desc
